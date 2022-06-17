7955dd4e6523cd167540798531fb34da707916ce
Mason Capps <masoncapps42119@gmail.com>
2022-06-17 12:28:27 -0500

    Adds all of the routes for the site controller as well as the index, show, create, update, and destroy actions

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,3 +1,36 @@
 class SitesController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:show]
+
+  def index
+    sites = current_user.sites
+    render json: sites.as_json
+  end
+
+  def show
+    site = Site.find_by(id: params[:id])
+    render json: site.as_json
+  end
+
+  def create
+    site = Site.new(
+      user_id: current_user.id,
+      name: params[:name],
+    )
+    site.save
+    render json: { message: "Site successfully created!" }
+  end
+
+  def update
+    site = Site.find_by(id: params[:id])
+    site.name = params[:name]
+
+    site.save
+    render json: { message: "Site successfully updated!" }
+  end
+
+  def destroy
+    site = Site.find_by(id: params[:id])
+    site.destroy
+    render json: { message: "Site successfully deleted!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # Session Routes
   post "/sessions" => "sessions#create"
-  
+
   # User Routes
   post "/users" => "users#create"
 
   # Site Routes
-
+  get "/sites" => "sites#index"
+  get "/sites/:id" => "sites#show"
+  post "/sites" => "sites#create"
+  patch "/sites/:id" => "sites#update"
+  delete "/sites/:id" => "sites#destroy"
   # Service Routes
 
   # Carted Service Routes
